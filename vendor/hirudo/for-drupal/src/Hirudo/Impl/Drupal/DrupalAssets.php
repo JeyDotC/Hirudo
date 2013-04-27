9f2f870bfbbc723eb008998455dd2846e21761a3
Jeysson Guevara <jeydotc@gmail.com>
2013-04-27 16:58:39 -0500

    Added application registering to separate apps from framework

--- vendor/hirudo/for-drupal/src/Hirudo/Impl/Drupal/DrupalAssets.php
+++ vendor/hirudo/for-drupal/src/Hirudo/Impl/Drupal/DrupalAssets.php
@@ -13,9 +13,18 @@ use Hirudo\Core\Annotations\Export;
  */
 class DrupalAssets extends Assets {
 
+    private static $appRegistry = array();
+
+    public static function registerApp($appName, $module, $src) {
+        self::$appRegistry[$appName] = array(
+            "module" => $module,
+            "src" => $src
+        );
+    }
+
     public function addCSS($cssPath, $external = false) {
         if (!$external) {
-            \drupal_add_css(\drupal_get_path("module", HIRUDO_DRUPAL_MODULE) . "/{$this->resolveLocalPath($cssPath)}", "file");
+            \drupal_add_css($this->resolveLocalPath($cssPath), "file");
         } else {
             \drupal_add_css($cssPath, "file");
         }
@@ -24,11 +33,31 @@ class DrupalAssets extends Assets {
 
     public function addJavaScript($jsPath, $external = false) {
         if (!$external) {
-            \drupal_add_js(\drupal_get_path("module", HIRUDO_DRUPAL_MODULE) . "/{$this->resolveLocalPath($jsPath)}", "file");
-        }else{
+            \drupal_add_js($this->resolveLocalPath($jsPath), "file");
+        } else {
             \drupal_add_js($jsPath, "file");
         }
         return "";
+    }
+
+    protected function resolveLocalPath($path) {
+        $result = $path;
+
+        if (strpos($path, "::") !== false) {
+            $parts = explode("::", $path);
+            $appName = $parts[0];
+            $asset = $parts[1];
+            
+            $registry = self::$appRegistry[$appName];
+            
+            $base = trim(str_replace("::", "/", $registry["src"]), "/");
+            
+            $result = \drupal_get_path("module", $registry["module"]) . "/$base/$appName/assets/$asset";
+        } else {
+            $result = "assets/$result";
+        }
+
+        return $result;
     }
 
 }
